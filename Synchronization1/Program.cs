4fef153dc0a90bf8b92e507d3afbd13a25996971
zbcjaso <zbcjaso@ZBC-S-6BN0W>
2022-05-02 10:35:17 +0200

    Add project files.

---
+++ Synchronization1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+
+namespace Synchronization1
+{
+    class Program
+    {
+        static int num = 0;
+        static readonly object lock1 = new object();
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(Add);
+            Thread t2 = new Thread(Subtract);
+            Thread.Sleep(1000);
+            t1.Start();
+            t2.Start();
+        }
+        static void Add()
+        {
+            while (true)
+            {
+                Monitor.Enter(lock1);
+                try
+                {
+                    num += 2;
+                    Console.WriteLine(num);
+                    Thread.Sleep(500);
+                }
+                finally
+                {
+                    Monitor.Exit(lock1);
+                }
+            }
+        }
+        static void Subtract()
+        {
+            while (true)
+            {
+                Monitor.Enter(lock1);
+                try
+                {
+                    num -= 1;
+                    Console.WriteLine(num);
+                    Thread.Sleep(500);
+                }
+                finally
+                {
+                    Monitor.Exit(lock1);
+                }
+            }
+        }
+    }
+}
